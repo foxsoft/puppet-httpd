8ed77b013ab54b90536382090a2dd4cdc7587f86
Omar Qureshi <omar@omarqureshi.net>
2011-08-25 23:10:11 +0100

    Move httpd logic here

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,4 +8,20 @@ class httpd {
     ensure => running,
     require => Package["httpd"]
   }
+
+  package {"httpd-devel":
+    ensure => installed,
+    require => Package["httpd"],
+  }
+
+  package {"mod_ssl":
+    ensure => installed,
+    require => Package["httpd"],
+  }
+
+  package {"mod_xsendfile":
+    ensure => installed,
+    require => Package["httpd"],
+  }
+
 }
