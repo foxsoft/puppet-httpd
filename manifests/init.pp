2de391b7d7c570142355e5f97c7e294be88328f2
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-09-04 21:02:27 +0100

    Politely ignoring package installation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,37 @@
 class httpd {
   
-  package {"httpd":
-    ensure => installed
+  if ! defined(Package["httpd"]) {
+    package {"httpd":
+      ensure => installed
+    }
   }
 
-  service {"httpd":
-    ensure => running,
-    require => Package["httpd"]
+  if ! defined(Service["httpd"]) {
+    service {"httpd":
+      ensure => running,
+      require => Package["httpd"]
+    }
   }
 
-  package {"httpd-devel":
-    ensure => installed,
-    require => Package["httpd"],
+  if ! defined(Package["httpd-devel"]) {
+    package {"httpd-devel":
+      ensure => installed,
+      require => Package["httpd"],
+    }
+  }
+  
+  if ! defined(Package["mod_ssl"]) {
+    package {"mod_ssl":
+      ensure => installed,
+      require => Package["httpd"],
+    }
   }
 
-  package {"mod_ssl":
-    ensure => installed,
-    require => Package["httpd"],
-  }
-
-  package {"mod_xsendfile":
-    ensure => installed,
-    require => Package["httpd"],
+  if ! defined(Package["mod_xsendfile"]) {
+    package {"mod_xsendfile":
+      ensure => installed,
+      require => Package["httpd"],
+    }
   }
 
 }
